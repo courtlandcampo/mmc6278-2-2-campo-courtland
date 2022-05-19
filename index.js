793b86bbb468ef783b9227efa85ca562a9584051
courtlandcampo <courtland.campo@ufl.edu>
2022-05-19 18:03:01 -0400

    Birthday Passed Alert Added

--- index.js
+++ index.js
@@ -3,8 +3,14 @@
 function greet(name) {
     var name = prompt("What is your name?");
     alert ("Hello," + name);
+    
     var userAge = prompt("How old are you?");
     var userAge = parseInt(userAge);
+    
     var birthday = confirm("Have you celebrated your birthday already this year?");
 
+    if (birthday = true) {
+        var yearP = new Date().getFullYear() - userAge;
+        alert("Based on my calculations, you were born in " + yearP);
+    } 
 }
